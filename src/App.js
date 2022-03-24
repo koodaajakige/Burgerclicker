109005329cdf07b62b8481349590b4ae0f6012f5
koodaajakige <kirsi.nykanen@edu.sasky.fi>
2022-03-24 13:39:10 +0200

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import { Route, BrowserRouter as Router } from 'react-router-dom';
+
+import Menu from './Menu';
+import Game from './Game';
+import Coupons from './Coupons';
+import Profile from './Profile';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Clicker extends Component {
+
+  render() {
+    return (
+      <Router>
+        <div className="clicker">
+          <Route path="/" exact component={Game} />
+          <Route path="/coupons" component={Coupons} />
+          <Route path="/profile" component={Profile} />
+        <Menu claimableCoupons={5}/>
+        </div>
+      </Router>
+    );
+  }
 }
 
-export default App;
+export default Clicker;
